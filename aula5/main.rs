4b1840825f09d94fa52960dda34e8d26c06a8de0
Otávio Rocha <94090679+OitavoRocha@users.noreply.github.com>
2023-06-28 18:06:15 -0300

    Add files via upload

---
+++ aula5/main.rs
@@ -0,0 +1,31 @@
+use rand::Rng;
+
+
+fn gera_vet(size: usize) -> Vec<i32> {
+    let mut rng = rand::thread_rng();
+    let mut vet = Vec::with_capacity(size);
+    for _ in 0..size {
+        vet.push(rng.gen_range(0..100));
+    }
+    vet
+}
+
+fn bubble_sort( vet: &mut Vec<i32> ) {
+    let n = vet.len();
+
+    for _i in 0..n-1 {
+        for j in 0.. n-1 {
+            if vet[j] > vet[j+1] {
+                vet.swap(j, j+1);
+            }
+        }
+    }
+}
+
+fn main() {
+    let size = 10;
+    let mut vet = gera_vet(size);
+    println!("Vetor original: {:?}", vet);
+    bubble_sort(&mut vet); // Opa... isso não existe (ainda)
+    println!("Vetor ordenado: {:?}", vet);
+}
